013fbecdd1c09712e95e08b489132e89322057a4
bcardone15 <brittneycardone@gmail.com>
2026-07-01 04:11:53 -0600

    Add Precision site configuration

---
+++ astro-site/src/config/site.ts
@@ -0,0 +1,15 @@
+export const site = {
+  name: 'Precision Window Tint',
+  legalName: 'Precision Window Tint',
+  url: 'https://precisionwindowtint.com',
+  email: 'sales.precisionwindowtint@gmail.com',
+  defaultPhone: '(702) 466-6435',
+  defaultPhoneHref: 'tel:+17024666435',
+  bookingUrl: import.meta.env.PUBLIC_BOOKING_URL || '/contact/',
+  description:
+    'Award-winning automotive, residential and commercial window tinting across Las Vegas, Henderson and Phoenix.',
+  socials: {
+    facebook: 'https://www.facebook.com/precisionwindowtint/',
+    instagram: 'https://www.instagram.com/precisionwindowtint/',
+  },
+};
